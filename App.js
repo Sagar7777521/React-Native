b2f1cfcee4093985f806e0d8ccb9c732cf65708a
Sagar Shekhar <70952463+Sagar7777521@users.noreply.github.com>
2023-12-20 23:14:34 +0530

    Tailwind, Screens and Other Routing Library Initiated

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
 import { View, Text, Image } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png");
+import React from "react";
 
-export default function app() {
+export default function Welcomescreen() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: "lightgreen",
-          padding: 60,
-        }}
-      ></View>
-      <Image
-        source={{ uri: "https://picsum.photos/300" }}
-        style={{ width: 300, height: 300, padding: 60 }}
-      ></Image>
+    <View>
+      <Text>Welcome Home!!!!!</Text>
     </View>
   );
 }
---
+++ screens/welcomescreen.js
@@ -0,0 +1,7 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+export default function Welcomescreen() {
+  return <SafeAreaView></SafeAreaView>;
+}
